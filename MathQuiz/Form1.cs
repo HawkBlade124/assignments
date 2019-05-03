68271836fddf6a7b74aa030038175ad1ff837876
Ben Fuller <Benfuller@fullerview.net>
2019-05-02 20:12:19 -0600

    Added code

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -10,11 +10,40 @@ using System.Windows.Forms;
 
 namespace MathQuiz
 {
-    public partial class Form1 : Form
+    public partial class form1 : Form
     {
-        public Form1()
+        //Initialize random number generator
+        //Create random numbers
+
+        Random randomizer = new Random();
+
+        //Store variables for addition problem
+        int addend1;
+        int addend2;
+        //Start the quiz and fill the question mark
+        public form1()
         {
-            InitializeComponent();
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+        }
+
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            //Convert the random numbers into strings
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            sum.Value = 0;
+        }
+
+        private void StartButton_Click_1(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
